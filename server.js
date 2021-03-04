103317565cf09adb004007cb9476bb5b72749539
UrsuRulz <burloiumarian@gmail.com>
2021-03-05 00:14:46 +0200

    set up almost complete

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
+const port 	  = 8080
 const express = require('express')
-const app = express()
+const app 	  = express()
 const bodyParser = require('body-parser')
 
 app.use( bodyParser.urlencoded({ extended: false }) )
 app.use( bodyParser.json() )
+
+app.use( '/app'   , express.static('static') )
+app.use( '/app/js', express.static('js'))
+
+app.listen( port , () => {
+   console.log(`Server listening on port ${port}`)
+})
